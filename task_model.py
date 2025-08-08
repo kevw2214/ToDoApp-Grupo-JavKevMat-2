1bc0ae0a8d7db25b635077c2695dd473bb74165d
Jherazor <javierandresherazo@gmail.com>
2025-08-08 17:22:18 -0500

    Añadir funcionalidad para eliminar tareas

--- task_model.py
+++ task_model.py
@@ -7,8 +7,9 @@ class TaskModel:
     def get_task_name(self):
         return self.task_name
     
-    def mark_as_completed(self):
-        self.is_completed = True
+    def delete_task(self):
+        self.task_name = None
+        self.is_completed = False
 
     def is_completed(self):
         return self.is_completed
